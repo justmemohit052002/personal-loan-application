d3a2229311eacbc13cd932a5d03a422ff355db39
madhavambulkar <madhvambulkar624@gmail.com>
2026-04-29 12:13:41 +0530

    resolved git conflict by keeping the upstream changes

--- src/main/java/com/loanapp/entity/User.java
+++ src/main/java/com/loanapp/entity/User.java
@@ -11,7 +11,6 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String fullName;
 
     @Column(unique = true, nullable = false)
